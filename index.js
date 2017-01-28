c914620498fb81689ad43f80dadaede02ac12787
Saurabh Raj <saurabhraj1896@gmail.com>
2017-01-28 18:47:52 +0530

    change

--- index.js
+++ index.js
@@ -9,6 +9,6 @@ io.on('connection', function(socket){
     io.emit('chat message', msg);
   });
 });
-http.listen(3000, function(){
-  console.log('listening on *:3000');
+http.listen(process.env.PORT || 3000, function(){
+  console.log('listening on', http.address().port);
 });
